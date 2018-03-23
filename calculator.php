c3989980a4a9fbbbdc3f140330d9eda7cdbcd57c
Lauren Cassidy <lauren.cassidy.nd@gmail.com>
2018-03-23 21:27:06 +0100

    added calculator.php script

---
+++ calculator.php
@@ -0,0 +1,34 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Calculator</title>
+</head>
+
+<body>
+
+
+<form>
+  <input type ="text" name="num1" placeholder="enter number 1">
+  <input type ="text" name="num2" placeholder="enter number 2">
+  <select name="operator">
+    <option>None</option>
+    <option>Add</option>
+    <option>Subtract</option>
+    <option>Multiply</option>
+    <option>Divide</option>
+  </select>
+  <br>
+  <button type="submit" value="submit" name="submit">Calculate</button>
+
+</form>
+
+
+
+
+
+
+  <script src="js/scripts.js"></script>
+</body>
+</html>
